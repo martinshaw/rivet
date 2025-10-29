4dff3d80568fff6e2e4073e73263e88979c12e0b
Martin Shaw <hello@martinshaw.co>
2025-10-29 05:04:33 +0000

    Complete installation of Filament and Filament versionable package

---
+++ config/rivet.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * All Rights Reserved, (c) 2025 rivet.
+ *
+ * Author:      Martin Shaw (developer@martinshaw.co)
+ * Created:     2025-10-29T04:55:09.583Z
+ * Modified:     2025-10-29T04:55:55.443Z
+ * File Name:   rivet.php
+ *
+ * Description: description
+ */
+
+return [
+
+    'brand_name' => env('BRAND_NAME', 'Example App'),
+
+];
--- database/migrations/2025_10_27_142125_add_filament_app_2fa_secret_column_to_users_table.php
+++ database/migrations/2025_10_27_142125_add_filament_app_2fa_secret_column_to_users_table.php
@@ -12,9 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')->after('password')->nullable();
-            $table->text('two_factor_recovery_codes')->after('two_factor_secret')->nullable();
-            $table->timestamp('two_factor_confirmed_at')->after('two_factor_recovery_codes')->nullable();
+            $table->text('app_authentication_secret')->nullable();
+            $table->text('app_authentication_recovery_codes')->nullable();
         });
     }
 
@@ -24,11 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-                'two_factor_confirmed_at',
-            ]);
+            $table->dropColumn('app_authentication_secret');
+            $table->dropColumn('app_authentication_recovery_codes');
         });
     }
 };
